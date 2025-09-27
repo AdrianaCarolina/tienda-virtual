d6d69fa4b421baf6d0b80b3fc25c249d102f9bb1
Sergio Ortiz <owes423349@gmail.com>
2025-09-26 19:47:54 -0500

    Some fixes and category management

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Product } from '../models/product.model';
 import { environment } from '../../enviroments/enviroment';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
@@ -18,13 +19,27 @@ export class ProductsService {
     return this.http.get<Product>(`${this.base}/${id}`);
   }
 
+  private generateId(min = 1000, max = 999999): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.base, product);
+    const payload: Product = {
+      ...product,
+      categoryId: Number(product.categoryId),
+    };
+
+    if (!payload.id) {
+      payload.id = this.generateId();
+    }
+
+    return this.http.post<Product>(this.base, payload).pipe(map((res) => res));
   }
 
   update(product: Product): Observable<Product> {
     if (!product.id) throw new Error('Product id required for update');
-    return this.http.put<Product>(`${this.base}/${product.id}`, product);
+    const payload = { ...product, categoryId: Number(product.categoryId) };
+    return this.http.put<Product>(`${this.base}/${product.id}`, payload);
   }
 
   delete(id: number): Observable<void> {
